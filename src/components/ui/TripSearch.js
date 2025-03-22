5632c8545e11f0962be10d2cd53c08c49d0a268c
daniel <danielhg1386@gmail.com>
2025-03-22 16:28:37 +0330

    feat: add static tour search interface

---
+++ src/components/ui/TripSearch.js
@@ -0,0 +1,48 @@
+"use client"
+
+import { useState } from "react"
+
+const TripSearch = () => {
+    const [tourType, setTourType] = useState("تورهای خارجی")
+
+    return (
+        <div className="bg-base-100 rounded p-4 shadow">
+            <div className="flex gap-5 mb-8">
+                {
+                    ["تورهای خارجی", "تورهای داخلی"].map(tour => (
+                        <button
+                            key={tour}
+                            className={tourType === tour ? "text-primary border-b-primary border-b-2 pb-2" : ""}
+                            onClick={e => setTourType(tour)}
+                        >{tour}</button>
+                    ))
+                }
+            </div>
+            
+            <form action="#">
+
+                <div className="flex gap-5">
+                    <select defaultValue="Pick a text editor" className="select select-primary w-full">
+                        <option disabled={true}>Pick a text editor</option>
+                        <option>VScode</option>
+                        <option>VScode fork</option>
+                        <option>Another VScode fork</option>
+                    </select>
+
+                    <select defaultValue="Pick a text editor" className="select select-primary w-full">
+                        <option disabled={true}>Pick a text editor</option>
+                        <option>VScode</option>
+                        <option>VScode fork</option>
+                        <option>Another VScode fork</option>
+                    </select>
+
+                    <button className="btn btn-primary px-10">
+                        جستجو
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default TripSearch
