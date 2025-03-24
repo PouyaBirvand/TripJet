ccc2c52d3f6c6b5de89e974b44300c8da423b3f5
daniel <danielhg1386@gmail.com>
2025-03-24 14:52:39 +0330

    feat: add all Home Components to HomePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,89 +1,23 @@
+import TripSearch from "../components/ui/TripSearch";
+import Hero from "../components/ui/Hero";
+import RecentlySearch from "../components/ui/RecentlySearch";
+import SpecialOffer from "../components/ui/SpecialOffer";
+import HomeAbout from "../components/ui/HomeAbout";
+import Comments from "../components/ui/Comments";
+import PopularTours from "../components/ui/PopularTours";
+
 const HomePage = () => {
-  return (
-    <div className="flex flex-col gap-5">
-      <div className=" flex gap-5 flex-wrap justify-center mt-10">
-        <div className="flex w-52 flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-            <div className="flex flex-col gap-4">
-              <div className="skeleton h-4 w-20"></div>
-              <div className="skeleton h-4 w-28"></div>
-            </div>
-          </div>
-          <div className="skeleton h-32 w-full"></div>
-        </div>
-        <div className="flex w-52 flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-            <div className="flex flex-col gap-4">
-              <div className="skeleton h-4 w-20"></div>
-              <div className="skeleton h-4 w-28"></div>
-            </div>
-          </div>
-          <div className="skeleton h-32 w-full"></div>
-        </div>
-        <div className="flex w-52 flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-            <div className="flex flex-col gap-4">
-              <div className="skeleton h-4 w-20"></div>
-              <div className="skeleton h-4 w-28"></div>
-            </div>
-          </div>
-          <div className="skeleton h-32 w-full"></div>
-        </div>
-        <div className="flex w-52 flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-            <div className="flex flex-col gap-4">
-              <div className="skeleton h-4 w-20"></div>
-              <div className="skeleton h-4 w-28"></div>
-            </div>
-          </div>
-          <div className="skeleton h-32 w-full"></div>
-        </div>
-        <div className="flex w-52 flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-            <div className="flex flex-col gap-4">
-              <div className="skeleton h-4 w-20"></div>
-              <div className="skeleton h-4 w-28"></div>
-            </div>
-          </div>
-          <div className="skeleton h-32 w-full"></div>
-        </div>
-        <div className="flex w-52 flex-col gap-4">
-          <div className="flex items-center gap-4">
-            <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-            <div className="flex flex-col gap-4">
-              <div className="skeleton h-4 w-20"></div>
-              <div className="skeleton h-4 w-28"></div>
-            </div>
-          </div>
-          <div className="skeleton h-32 w-full"></div>
-        </div>
-      </div>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage:
-            'url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)',
-        }}
-      >
-        <div className="hero-overlay"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-              exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <Hero />
+            <TripSearch />
+            <RecentlySearch />
+            <SpecialOffer />
+            <HomeAbout />
+            <Comments />
+            <PopularTours />
+        </>
+    );
 };
 
 export default HomePage;
