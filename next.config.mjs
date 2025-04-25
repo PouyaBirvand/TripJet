214b7a3e2a60630344a3ace427334480b8acdfd1
PouyaBirvand <pooyabirvand@gmail.com>
2025-04-25 17:58:59 +0330

    Update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['picsum.photos','example.com','source.unsplash.com'],
+        domains: ['picsum.photos'],
       },
 };
 
